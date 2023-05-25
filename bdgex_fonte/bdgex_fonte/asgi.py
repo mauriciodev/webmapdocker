ecec7c4b9dbf53e4b816697ba69cf0130d467894
mauricio.dev <mauricio.dev@gmail.com>
2023-05-25 09:40:06 -0300

    bdgex_fonte

---
+++ bdgex_fonte/bdgex_fonte/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bdgex_fonte project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bdgex_fonte.settings")
+
+application = get_asgi_application()
