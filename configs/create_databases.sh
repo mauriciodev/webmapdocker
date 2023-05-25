da37d89f3aeaae48512b3ebd882763f9e8d5341a
mauriciodev <mauricio.dev@gmail.com>
2023-05-25 17:27:23 -0300

    Incomplete. I still gotta think about pycsw and django integration.

--- configs/create_databases.sh
+++ configs/create_databases.sh
@@ -8,3 +8,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE DATABASE geo100 TEMPLATE template_postgis;
 	CREATE DATABASE geo250 TEMPLATE template_postgis;
 EOSQL
+
+
+docker-compose run mdmanager python manage.py collectstatic
+docker-compose run mdmanager python manage.py makemigrations
+docker-compose run mdmanager python manage.py makemigrations mdmanager
+docker-compose run mdmanager python manage.py migrate
+docker-compose run mdmanager python manage.py createsuperuser --username mauricio
--- metadatamanager/metadatamanager/settings.py
+++ metadatamanager/metadatamanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zkt^^bar)48y*q(q2y0d@p93=v$t^7ri84*9vk()m5x*qba0a3'
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get("DEBUG", default=1))
+
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+
+#ALLOWED_HOSTS = ['mdmanager']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default='localhost').split(" ")
 
 
 # Application definition
@@ -76,10 +84,22 @@ WSGI_APPLICATION = 'metadatamanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
